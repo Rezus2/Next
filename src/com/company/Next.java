e68ede10e829fd1988b2901fe3c8a0d7a31202ab
Roman <55631436+Rezus2@users.noreply.github.com>
2019-09-27 23:12:25 +0300

    Update Next.java

--- src/com/company/Next.java
+++ src/com/company/Next.java
@@ -6,8 +6,8 @@ public class Next {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int x=in.nextInt();
-        int a=x+((x+1)%2)+1;
+        int n=in.nextInt();
+        int a=n+((n+1)%2)+1;
         System.out.println(a);
     }
 }
